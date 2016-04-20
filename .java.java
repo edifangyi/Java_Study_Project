6aab7bbc4189b07efd3b7f88be7377c96aff0cdb
FANG <fangyi1896@gmail.com>
2016-04-20 21:42:15 +0800

    study

--- .java.java
+++ .java.java
@@ -9,6 +9,713 @@ JVM( Java Virtual Machine ) ：跨平台通过 JVM实现的
  
  */
 
+1.认识Java标识符
+
+	标识符可以由字母、数字、下划线（_）、美元符（$）组成，但不能包含 @、%、空格等其它特殊字符，不能以数字开头。譬如：123name 就是不合法滴
+
+2.  标识符不能是 Java 关键字和保留字（ Java 预留的关键字，以后的升级版本中有可能作为关键字），但可以包含关键字和保留字。如：不可以使用 void 作为标识符，但是 Myvoid 可以
+
+3.  标识符是严格区分大小写的。 所以涅，一定要分清楚 imooc 和 IMooc 是两个不同的标识符哦！
+
+4.  标识符的命名最好能反映出其作用，做到见名知意。
+
+
+
+
+
+5.Java 语言是一种强类型语言。通俗点说就是，在 Java 中存储的数据都是有类型的，而且必须在编译时就确定其类型。 
+
+		 String     name="爱慕课";
+		 char     sex='男';
+		 int     num=18;
+		 double     price=120.5;
+		 boolean     isOK=true;
+
+
+
+6.Java 中变量的使用规则
+
+	1、Java 中的变量需要先声明后使用
+	2、变量使用时，可以声明变量的同时进行初始化，也可以先声明后赋值
+	3、变量中每次只能赋一个值，但可以修改多次
+	4、main 方法中定义的变量必须先赋值，然后才能输出
+
+
+7.Java 中的强制类型转换
+		double heightAvg1=176.2;
+		int heightAvg2=(int)heightAvg1;
+
+
+8.Java 常量
+	语法：final 常量名 = 值;
+
+
+9.Java 数组
+
+		// 定义一个数组，保存五名学生的成绩
+		int[] scores = { 78, 93, 97, 84, 63 };
+
+		// 输出数组中的第二个成绩
+		System.out.println("数组中的第2个成绩为：" +   scores[1]         );
+	1、 声明数组
+
+	语法：  数据类型[ ] 数组名；
+ 				
+ 		int[] scores;
+ 		double height[];
+ 		String[] name;
+	
+	或者   数据类型 数组名[ ]；
+    
+    2、 分配空间
+
+	简单地说，就是指定数组中最多可存储多少个元素
+
+	语法：  数组名 = new  数据类型 [ 数组长度 ];   
+
+ 		int[] scores =  new String[5];  
+
+	
+	4、 处理数组中数据
+ 		int[] scores={1,2,3,4,5};
+ 	等价
+ 		int scores = new int[]{1,2,3,4,5};
+
+
+
+10.使用循环操作 Java 中的数组
+
+		// 定义一个长度为 3 的字符串数组，并赋值初始值
+		String[] hobbys = { "sports", "game", "movie" };
+		System.out.println("循环输出数组中元素的值：");
+		
+		// 使用循环遍历数组中的元素
+		for(int i = 0; i <hobbys.length; i++) {
+    	    System.out.println(hobbys[i]);
+		}
+
+	1. 数组名.length  用于获取数组的长度
+	2. 数组下标的范围是 0 至 数组长度-1 ，如果越界访问，就会报错
+
+
+		
+11.使用 Arrays 类操作 Java 中的数组
+
+	Arrays 类是 Java 中提供的一个工具类，在 java.util 包中。
+	该类中包含了一些方法用来直接操作数组，比如可直接实现数组的排序、搜索等
+
+		// 定义一个字符串数组
+		String[] hobbys = { "sports", "game", "movie" };
+        
+		// 使用Arrays类的sort()方法对数组进行排序
+		Arrays.sort(hobbys);
+        
+		// 使用Arrays类的toString()方法将数组转换为字符串并输出
+		System.out.println( Arrays.toString(hobbys) );
+
+
+12.使用 foreach 操作数组
+
+	foreach 并不是 Java 中的关键字，是 for 语句的特殊简化版本，在遍历数组、集合时，
+	foreach 更简单便捷。从英文字面意思理解 foreach 也就是“ for 每一个”的意思，
+
+	for( 元素类型 元素变量:遍历对象) {
+		执行的代码
+	}
+        
+			// 定义一个整型数组，保存成绩信息
+			int[] scores = { 89, 72, 64, 58, 93 };
+	        
+			// 对Arrays类对数组进行排序
+			Arrays.sort(scores);
+	        
+			// 使用foreach遍历输出数组中的元素
+			for ( int scores :scores     ) {
+				System.out.println(score);
+			}
+
+13.Java 中的二维数组
+	
+	
+	1.声明数组并分配空间
+
+		数据类型[][] 数组名 = new 数据类型[行][列];
+
+		数据类型[][] 数据名;
+		数组名 = new 数据类型[行][列];
+
+	2.赋值
+
+		二维数组的赋值，和一维数组类似，可以通过下标来逐个赋值，注意索引从 0 开始	
+
+		数据类型[][] 数组名 = {{1,2},{11,22},{21,22}};	
+
+
+		// 定义两行三列的二维数组并赋值
+		         names={{"tom","jack","mike"},{"zhangsan","lisi","wangwu"}};
+                 
+		// 通过二重循环输出二维数组中元素的值
+		for (int i = 0; i < numes.length    ; i++) {
+            
+			for (int j = 0; j < names[i].length; j++) {
+                
+				System.out.println( numes[i][j] );
+			}
+            
+			System.out.println();
+		}
+
+	3.
+		int[][] num = new int[3][]; //定义一个三行的二维数组
+		num[0] = new int[2];	//为第一行分配两列
+		num[1] = new int[3];	//为第二行分配三列
+		num[2] = new int[4];	//为第三行分配四列
+
+		num[0][0] = 1;	//第一行第一列赋值1
+		num[1][1] = 2;	//第二行第二列赋值2
+		num[2][2] = 3;	//第三行第四列赋值3
+
+
+14.如何定义 Java 中的方法
+
+	所谓方法，就是用来解决一类问题的代码的有序组合，是一个功能模块。
+
+	一般情况下，定义一个方法的语法是：
+
+
+
+	其中：
+
+	1、 访问修饰符：方法允许被访问的权限范围， 可以是 public、protected、private 甚至可以省略 ，其中 public 表示该方法可以被其他任何代码调用，其他几种修饰符的使用在后面章节中会详细讲解滴
+
+	2、 返回值类型：方法返回值的类型，如果方法不返回任何值，则返回值类型指定为 void ；如果方法具有返回值，则需要指定返回值的类型，并且在方法体中使用 return 语句返回值
+
+	3、 方法名：定义的方法的名字，必须使用合法的标识符
+
+	4、 参数列表：传递给方法的参数列表，参数可以有多个，多个参数间以逗号隔开，每个参数由参数类型和参数名组成，以空格隔开 
+
+	根据方法是否带参、是否带返回值，可将方法分为四类：
+
+	Ø 无参无返回值方法
+
+	Ø 无参带返回值方法
+
+	Ø 带参无返回值方法
+
+	Ø 带参带返回值方法
+
+
+
+15.Java 中无参无返回值方法的使用
+
+	如果方法不包含参数，且没有返回值，我们称为无参无返回值的方法。
+
+	方法的使用分两步：
+
+	第一步，定义方法
+
+	例如：下面代码定义了一个方法名为 show ，没有参数，且没有返回值的方法，执行的操作为输出 “ welcome to imooc. ”
+
+		public void show() {
+			XXX;
+		}
+
+	注意哦：
+
+	1、 方法体放在一对大括号中，实现特定的操作
+
+	2、 方法名主要在调用这个方法时使用，需要注意命名的规范，一般采用第一个单词首字母小写，其它单词首字母大写的形式
+
+	第二步，调用方法
+
+	当需要调用方法执行某个操作时，可以先创建类的对象，然后通过  对象名.方法名();  来实现（关于类和对象的概念在后面章节中会详细讲解滴，先熟悉语法，表着急哦~~）
+
+
+
+16.Java 中无参带返回值方法的使用
+
+	public int calSum() {
+		int sum=0;
+		return sum;
+	}
+
+	在 calSum( ) 方法中，返回值类型为 int 类型，因此在方法体中必须使用 return 返回一个整数值。
+
+	调用带返回值的方法时需要注意，由于方法执行后会返回一个结果，因此在调用带返回值方法时一般都会接收其返回值并进行处理。如：
+
+	
+	1、 如果方法的返回类型为 void ，则方法中不能使用 return 返回值！
+	2、 方法的返回值最多只能有一个，不能返回多个值
+	3、 方法返回值的类型必须兼容，例如，如果返回值类型为 int ，则不能返回 String 型值
+
+
+17.Java 中带参无返回值方法的使用
+
+	1、 调用带参方法时，必须保证实参的数量、类型、顺序与形参一一对应
+	2、 调用方法时，实参不需要指定数据类型，如 
+	3、 方法的参数可以是基本数据类型，如 int、double 等，也可以是引用数据类型，如 String、数组等
+	4、 当方法参数有多个时，多个参数间以逗号分隔
+
+
+18.Java 中方法的重载
+	
+	如果同一个类中包含了两个或两个以上方法名相同、方法参数的个数、顺序或类型不同的方法，则称为方法的重载，也可称该方法被重载了。
+
+	判断方法重载的依据：
+
+1、 必须是在同一个类中
+
+2、 方法名相同
+
+3、 方法参数的个数、顺序或类型不同
+
+4、 与方法的修饰符或返回值没有关系
+
+
+/**
+ 
+ */
+
+1.如何定义一个类：
+	1.类的重要性：所有java程序都是以类class为组织单元的，是基本元素。
+	2.类是模子，确定对象将会拥有什么样的特征（属性）和行为（方法）。先有类才能够描述具体对象。
+	3.类的组成：属性和方法
+	4.定义一个类的顺序：
+		a.定义类名：
+			publi class 类名 {
+				内容
+			}
+			class是定义类的关键字。
+
+		b.编写类的属性：在{}内定义不同的属性类型，可以是一种可以是N种。
+
+		c.编写类的方法：可以多种行为，可以一种。
+
+
+2.
+	成员变量：在类中定义，用来描述对象将要有什么（属性）。可被本类方法和其他跟当前类有关系的方法使用。
+	局部变量：在类的方法中定义，在方法中临时保存数据。（只能在当前的方法中使用）
+	
+	区别：
+	1.作用域不同：局部变量的作用域仅限于定义它的方法，成员变量的作用域在整个类内部都是可见的。
+	2.初始值不同：java会给成员变量一个初始值，java不会给局部变量赋予初始值。
+	3.在同一个方法中，不允许有同名局部变量；在不同的方法中，可以有同名局部变量。
+	4.全局变量和局部变量同名时，局部变量具有更高的优先级。
+
+
+
+3.构造方法：
+
+	1.使用new+构造方法=创建新对象
+	
+	2.用于定义类中初始化对象的方法
+	（构造也是方法的一种）
+	
+	3.与类同名且没有返回值
+		public 构造方法名（）{
+
+		}
+
+	4.语法格式：
+
+		public 构造方法名(可以指定参数){
+			初始化代码
+		}
+	
+	5.特点：
+		a.如果没有指定构造方法，系统默认添加无参方法
+		b.如果指定了构造方法，就按照指定的添加
+	
+	6.构造方法也有重载（根据参数决定使用哪个方法）
+	
+	7.构造方法中可以加入适当的判断，避免赋值不恰当
+	
+	通过有参构造方法创建对象，并给对象中的实例变量赋初始值
+	
+	Telphone phone2=new Telphone(值1，值2，值3);
+	
+	当有指定构造方法，无论是有参，还是无参的构造方法，都不会自动添加无参的构造方法
+	构造方法的重载：方法名相同，但参数不同的多个方法，调用时会自动根据不同的参数选择相应的方法
+
+/**
+ 
+ */
+
+
+4.Java 中的 static 使用之静态变量
+
+	1.Java 中被 static 修饰的成员称为静态成员或类成员。它属于整个类所有，而不是某个对象所有，即被类的所有对象所共享。
+		静态成员可以使用类名直接访问，也可以使用对象名进行访问。当然，鉴于他作用的特殊性更推荐用类名访问~~
+	
+	2.
+		static StringBuilder hobby = "AAA";
+		
+		//静态变量 可以直接使用类名访问，无序创建类的对象
+		HelloWorld.hobby;
+
+		//创建类的对象
+		HelloWorld hello = new HelloWorld();
+		//使用对象名来访问静态变量
+		hello.hobby;
+
+		//使用对象名的形式修改静态变量值
+		hello.hobby = "BBB";
+
+		//在此使用类名访问静态变量，值已经被修改
+		HelloWorld.hobby;//输出 BBB
+	
+
+	要注意哦：静态成员属于整个类，当系统第一次使用该类时，就会为其分配内存空间直到该类被卸载才会进行资源回收！~~
+
+
+5.Java 中的 static 使用之静态方法
+
+	public class HelloWorld {
+		//使用 static 关键字 声明静态方法
+		public static void print() {
+			;
+		}
+
+		public static void main(String[] args) {
+			//直接使用类名调用静态方法
+			HelloWorld.println();
+
+			//通过对象名调用，当然更推荐类名调用的方式
+			HelloWorld hello = new HelloWorld();
+			hello.print();
+		}
+	}
+
+
+
+	需要注意：
+
+	1、 静态方法中可以直接调用同类中的静态成员，但不能直接调用非静态成员。如：
+
+			public class HelloWorld {
+				String name = "AAA"; //非静态变量name;
+				static String hobby = "BBB"; //静态变量 hobby
+
+				//在静态方法调用非静态变量
+				public static void print() {
+					name;//不能直接调用
+					hobby;//可以直接调用静态变量
+				}
+			}
+			
+
+		如果希望在静态方法中调用非静态变量，可以通过创建类的对象，然后通过对象来访问非静态变量。如
+			public static void print() {		
+				HelloWorld hello = new HelloWorld();				
+				hello.name;
+			}
+
+	2、 在普通成员方法中，则可以直接访问同类的非静态变量和静态变量，
+
+			public void show() {		
+				name;
+				hobby;
+			}
+
+	3、 静态方法中不能直接调用非静态方法，需要通过对象来访问非静态方法。如：
+
+			//普通成员方法
+			public void show() {
+				;
+			}
+			//静态方法
+			public static void print() {
+				;
+			}
+			public static void main(String[] args) {
+				//普通成员方法通过对象调用
+				HelloWorld hello = new HelloWorld();
+				hello.show();
+
+				//可以直接调用静态方法
+				print();
+			}
+
+
+6.Java 中的 static 使用之静态初始化块
+
+	public class HelloWorld {
+	    
+	    String name; // 声明变量name
+		String sex; // 声明变量sex
+		static int age;// 声明静态变量age
+	    
+	    // 构造方法
+		public HelloWorld() { 
+			System.out.println("通过构造方法初始化name");
+			name = "tom";
+		}
+	    
+	    // 初始化块
+		{ 
+			System.out.println("通过初始化块初始化sex");
+			sex = "男";
+		}
+	    
+	    // 静态初始化块
+		static { 
+			System.out.println("通过静态初始化块初始化age");
+			age = 20;
+		}
+	    
+		public void show() {
+			System.out.println("姓名：" + name + "，性别：" + sex + "，年龄：" + age);
+		}
+	    
+		public static void main(String[] args) {
+	        
+	        // 创建对象
+			HelloWorld hello = new HelloWorld();
+			// 调用对象的show方法
+	        hello.show();
+
+	        HelloWorld hello2 = new HelloWorld();
+	        
+		}
+	}
+
+	1.	在类的声明中，可以包含多个初始化块，当创建类的实例时，就会依次执行这些代码块。
+		如果使用 static 修饰初始化块，就称为静态初始化块。
+
+	2.	静态初始化块只在类加载时执行，且只会执行一次，同时静态初始化块只能给静态变量赋值，不能初始化普通的成员变量。
+
+	3.	程序运行时静态初始化块最先被执行，然后执行普通初始化块，最后才执行构造方法。
+		由于静态初始化块只在类加载时执行一次，所以当再次创建对象 hello2 时并未执行静态初始化块。
+
+/**
+ 
+ */
+封装：将类的某些信息隐藏在类内部，不允许外部程序直接访问，而是通过该类提供的方法来实现对隐藏信息的操作和访问。
+
+好处：
+	1、只能通过规定的方法访问数据。
+	2、隐藏类的实例细节，方便修改和实现
+
+
+封装的步骤：
+
+	1、修改属性的可见性，设为 private
+	2、创建个 getter/setter 方法用于属性的读写
+	3、在 getter/setter 方法中加入属性控制语句，对属性值的合法性进行判断
+	
+
+	众所周知，JAVA是面向对象的一门语言，每个对象都有各自的属性和方法．但是有的属性涉及安全因素或秘密因素不能让外面的程序调用，所以就用到了private,
+	经过它定义的变量只能在此对象内部调用，外部程序就无法调用，也就是说只能供其自身调用.
+
+
+	1.包的作用：
+		管理java文件
+		解决同名文件冲突
+
+	2.定义包：package包名
+		注：必须放在java源程序的第一行
+		包名间可以使用"."号隔开
+		eg:com.imooc.MyClass
+
+	3.系统中的包
+		java.(功能).(类)
+		java.lang.(类)包含java语言基础的类
+		java.util.(类)包含java语言中各类工具类
+		java.io.(类)包含输入、输出相关功能的类
+
+	4.包的使用
+		1）可以使用import关键字，在某个文件使用其他文件中的类。
+		import com.imooc.music.MyClass
+		2）java中，包的命名规范是全小写字母拼写
+		3）使用的时候不但可以加载某个包下的所有文件
+		eg：com.imooc.*
+		也可以加载某个具体子包下的所有文件
+		eg:com.imooc.music.*
+
+
+
+2.Java 中的访问修饰符
+
+	public-------任何地方的访问
+	protected----同类、同包、子类
+	默认(没写)---同类、同包
+	private------同类
+
+
+
+3.Java 中的 this 关键字
+
+	1. 若在Eclipse中，类里创建了private属性，这里可以利用Eclipse快捷键自动创建get set方法. 
+		
+		(点击Eclipse ， sources(源码) ,下面有一个setter...代表自动创建入口方法)
+
+	2.自动创建后，属性名往往与参数名相同，为了区分，就用到this关键字. this.screen代表当前对象的属性.
+		
+		this.方法名()则代表调用当前对象的方法.
+
+
+
+	This关键字： 
+		（1）作用：本类一个对象的引用； 
+
+		（2）调用方式：this.属性（成员变量） this.方法（成员方法）。 
+
+		（3）什么情况下使用：当成员变量被局部变量或方法的参数覆盖掉时，使用this关键字明确引用的是成员变量还是局部变量还是方法参数。
+
+
+
+ 4.什么是 Java 中的内部类 
+
+	1.内部类（ Inner Class ）就是定义在另外一个类里面的类。与之对应，包含内部类的类被称为外部类。
+
+	2.内部类的主要作用如下：
+
+		1. 内部类提供了更好的封装，可以把内部类隐藏在外部类之内，不允许同一个包中的其他类访问该类
+
+		2. 内部类的方法可以直接访问外部类的所有数据，包括私有的数据
+
+		3. 内部类所实现的功能使用外部类同样可以实现，只是有时使用内部类更方便
+
+	3.内部类可分为以下几种：
+
+		成员内部类
+		静态内部类
+		方法内部类
+		匿名内部类
+
+	//外部类HelloWorld
+	public class HelloWorld {
+	    
+	    // 内部类Inner，类Inner在类HelloWorld的内部
+	    public class Inner {
+	        
+			// 内部类的方法
+			public void show() {
+				System.out.println("welcome to imooc!");
+			}
+		}
+	    
+		public static void main(String[] args) {
+	        
+	        // 创建外部类对象
+			HelloWorld hello = new HelloWorld();
+	        // 创建内部类对象
+			Inner i = hello.new Inner();
+	        // 调用内部类对象的方法
+			i.show();
+		}
+	}
+
+
+5.Java 中的成员内部类
+
+	//外部类HelloWorld
+	public class HelloWorld{
+	    
+	    //外部类的私有属性name
+	    private String name = "imooc";
+	    
+	    //外部类的成员属性
+	    int age = 20;
+	    
+		//成员内部类Inner
+		public class Inner {
+			String name = "爱慕课";
+			
+			int age = 30;//如果外部类和内部类具有相同的成员变量或方法，内部类默认访问自己的成员变量或方法，
+
+	        //内部类中的方法
+			public void show() { 
+				System.out.println("外部类中的name：" + HelloWorld.this.name );
+				System.out.println("内部类中的name：" + name );
+				System.out.println("外部类中的age：" + age);
+			}
+		}
+	    
+		//测试成员内部类
+		public static void main(String[] args) {
+	        
+	        //创建外部类的对象
+			HelloWorld o = new HelloWorld (); 
+	        
+	        //创建内部类的对象
+			Inner inn =   o.new Inner()           ;
+	        
+	        //调用内部类对象的show方法
+			inn.show();
+		}
+	}
+
+	外部类中的name：imooc
+	内部类中的name：爱慕课
+	外部类中的age：20
+
+	1、 外部类是不能直接使用内部类的成员和方法滴
+
+		可先创建内部类的对象，然后通过内部类的对象来访问其成员变量和方法。
+
+	2、 如果外部类和内部类具有相同的成员变量或方法，内部类默认访问自己的成员变量或方法，
+
+		如果要访问外部类的成员变量，可以使用 this 关键字。如：
+			HelloWorld.this.age;
+
+
+6.Java 中的静态内部类
+
+	静态内部类是 static 修饰的内部类，这种内部类的特点是：
+
+		1、 静态内部类不能直接访问外部类的非静态成员，但可以通过 new 外部类().成员 的方式访问 
+
+		2、 如果外部类的静态成员与内部类的成员名称相同，可通过 “ 类名.静态成员 ” 访问外部类的静态成员；
+			如果外部类的静态成员与内部类的成员名称不相同，则可通过 “ 成员名 ” 直接调用外部类的静态成员
+
+		3、 创建静态内部类的对象时，不需要外部类的对象，可以直接创建 内部类 对象名= new 内部类();
+
+	//外部类
+	public class HelloWorld {
+	    
+	    // 外部类中的静态变量score
+	    private static int score = 84;
+	    
+	    // 创建静态内部类
+		public static class SInner {
+	        // 内部类中的变量score
+	        int score = 91;
+	        
+			public void show() {
+				System.out.println("访问外部类中的score：" + HelloWorld.score );
+				System.out.println("访问内部类中的score：" + score);
+			}
+		}
+
+		// 测试静态内部类
+		public static void main(String[] args) {
+			
+			// 直接创建内部类的对象
+	        SInner si = new SInner();
+	        // 调用show方法
+			si.show();
+		}
+	}
+
+
+	访问外部类中的score：84
+	访问内部类中的score：91
+
+
+
+
+
+
+
+
+
+
+
+/**
+ 
+ */
+
 //在方法体外，类体内声明的变量成为成员变量
 public void method(){
 	int i;
